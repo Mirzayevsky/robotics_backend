f86710fd0b3d70c1997a27bc8a4c2378b88a3453
mirzayevsky <mirzayevsky@gmail.com>
2022-11-28 23:22:13 +0500

    fixed bugs

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const app = express();
+var cors = require('cors')
+app.use(cors())
 require('./startup/routes')(app);
 require('./startup/db')();
 require('./startup/config')();
