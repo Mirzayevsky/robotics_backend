f177c2fcb9666d32f61673e9480a8d751b68c3ba
Sirojiddin Mirzayev <79570217+Mirzayevsky@users.noreply.github.com>
2023-11-27 00:45:16 +0500

    Update config.js
    Comments added

--- startup/config.js
+++ startup/config.js
@@ -1,5 +1,5 @@
 const config = require('config');
-
+// module of jwt key
 module.exports = function () {
     if (!config.get('jwtPrivateKey')) {
         throw new Error('FATAL ERROR: environment variable robtics22_jwtPrivateKey is not defined.');
